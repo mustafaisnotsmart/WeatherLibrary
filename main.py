d0c6d82a9e762448d75a79c98d6db654cb9cc50d
mustafa <89872617+mustafaisnotsmart@users.noreply.github.com>
2022-02-04 13:43:32 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from weather import *
+
+print(temperature("Dubai"))
+
---
+++ weather/__init__.py
@@ -0,0 +1,27 @@
+import re
+import string
+import requests
+__version__ = "1.0.0"
+import json
+
+
+token = "101d9b88089c209d67d9ae493ba1f4c7"
+def temperature(location):
+    res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={token}")
+    
+    weather = res.text
+    dict=json.loads(weather)
+    betterdict = dict["main"]
+    temperature = betterdict["temp"]
+    farhenhiettemp = 9 / 5 * (temperature - 273) + 32
+    return int(farhenhiettemp)
+    
+def condition(location):
+    res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={token}")
+    
+    weather = res.text
+    dict=json.loads(weather)
+    betterdict = dict["main"]
+    temperature = betterdict["temp"]
+    farhenhiettemp = 9 / 5 * (temperature - 273) + 32
+    return int(farhenhiettemp)
