aa6a3813cec8536c4959b3a5470bd600dc77c7d9
mustafa <89872617+mustafaisnotsmart@users.noreply.github.com>
2022-02-10 14:14:51 -0700

    Added Description cuz

--- setup.py
+++ setup.py
@@ -2,7 +2,34 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="WeatherLookup",
-    version="1.0.0",
+    long_description="""# Weather Lookup
+
+Lookup the live weather data to any city!
+
+## Valid Queries
+
+- City name 
+- State name
+- Zipcode
+
+
+## Installation
+
+``` Bash
+pip install WeatherLookup
+```
+
+## Usage 
+
+```Python
+import WeatherLookup
+
+DubaiWeather = WeatherLookup.temperature('Dubai')
+print(DubaiWeather)
+
+```""",
+    long_description_content_type='text/markdown',
+    version="1.0.1",
     description="Look up live weather data from across the world",
     author="Mustafa A",
     url="https://github.com/mustafaisnotsmart/WeatherLibrary",
